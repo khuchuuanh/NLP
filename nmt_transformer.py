f97ad32072d5e27af48ea143b093dbfc18c86782
KhucHuuAnh <khuchuuanh@gmail.com>
2023-02-06 08:45:03 +0700

    fix

--- nmt_transformer.py
+++ nmt_transformer.py
@@ -11,16 +11,16 @@ class DataPreparing:
             print('Create Folder')
             os.mkdir(self.save_data_dir)
 
-        if len(os.listdir(self.save_data_dir)) ==0:
+        if len(os.listdir(self.save_data_dir)) == 0:
             print('#1-Download Dataset')
-            corpus = datasets.load_datasets('mt_eng_vietnamese', 'iwslt2015-en-vi')
+            corpus = datasets.load_dataset('mt_eng_vietnamese', 'iwslt2015-en-vi')
             # corpus = datasets.load_datasets('wmt14','de-en')
             source_train, target_train = self.get_data(corpus['train'])
             source_valid, target_valid = self.get_data(corpus['validation'])
             source_test, target_test   = self.get_data(corpus['test'])
 
             print('Source lang : {} - train : {}, valid : {}, test : {}'.format(self.source_lang, len(source_train), len(source_valid), len(source_test)))
-            print('Target lang : {} - train : {}, valid : {}, test : {}'.format(self.target_lang),len(target_train), len(target_valid),len(target_test))
+            print('Target lang: {} - train: {}, valid: {}, test: {}'.format(self.target_lang, len(target_train), len(target_valid), len(target_test)))
 
             print('#2 - Save dataset')
             self.save_data(source_train, os.path.join(self.save_data_dir, 'train.' + self.source_lang ))
@@ -28,7 +28,7 @@ class DataPreparing:
             self.save_data(source_test, os.path.join(self.save_data_dir,'test.'+ self.source_lang))
             self.save_data(target_train, os.path.join(self.save_data_dir, 'train.' +self.target_lang))
             self.save_data(target_valid, os.path.join(self.save_data_dir, 'valid.' + self.target_lang))
-            self.save_data(target_test, os.path.join(self.save_data_dir),'test' + self.target_lang)
+            self.save_data(target_test, os.path.join(self.save_data_dir,'test.' + self.target_lang))
         
         else:
             print('Dataset exist!')
@@ -37,13 +37,13 @@ class DataPreparing:
         source_data = []
         target_data = []
         for data in corpus:
-            source_data.appned(self.preprocess_text(data['translation'][self.source_lang], self.source_lang))
+            source_data.append(self.preprocess_text(data['translation'][self.source_lang], self.source_lang))
             target_data.append(self.preprocess_text(data['translation'][self.target_lang], self.target_lang))
         return source_data, target_data
 
     def preprocess_text(self, text, lang = None):
         text = text.replace('&quot;', ' ')
-        text = text.replace('&apos', ' ')
+        text = text.replace('&apos', "'")
         if lang == 'en':
             text = contractions.fix(text)
         text = text.lower()
